7af7bc12ab32643fa17e059c173a951771d828fd
мега шланг <megashlanggggg@gmail.com>
2024-04-17 16:21:35 +0500

    Initial commit

---
+++ app/src/main/java/com/example/testtaskavia/presentation/ui/ShortFragment.kt
@@ -0,0 +1,29 @@
+package com.example.testtaskavia.presentation.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.testtaskavia.databinding.FragmentShortBinding
+
+class ShortFragment : Fragment() {
+
+    private var _binding: FragmentShortBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentShortBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/testtaskavia/presentation/ui/SubscriptionsFragment.kt
@@ -0,0 +1,28 @@
+package com.example.testtaskavia.presentation.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.testtaskavia.databinding.FragmentSubscriptionsBinding
+
+class SubscriptionsFragment : Fragment() {
+
+    private var _binding: FragmentSubscriptionsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSubscriptionsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
